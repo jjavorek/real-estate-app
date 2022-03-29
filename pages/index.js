16ae6aec70a45683faebb05dee43fdc7c11d3313
jjavorek <justinjavorek@gmail.com>
2022-03-29 07:28:53 -0400

    simple banner component

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,18 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
+const Banner = ({ purpose }) => (
+  <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+    {purpose}
+  </Flex>
+);
+
 export default function Home() {
   return (
     <div>
       <h1>Justin</h1>
+      <Banner purpose={"For Sale"} />
+      <Banner purpose={"For Rent"} />
     </div>
   );
 }

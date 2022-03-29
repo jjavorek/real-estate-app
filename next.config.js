2486047ae7204fbacc910fdfe476d52e73ae3c93
jjavorek <justinjavorek@gmail.com>
2022-03-29 07:58:24 -0400

    banners + next config for images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+  images: {
+    domains: ["bayut-production.s3.eu-central-1.amazonaws.com"],
+  },
+};
